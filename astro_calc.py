2758fae3e84a45afc401d0db7de0cd17d4e60a41
NadezdaSev <nadezdavoice2025@gmail.com>
2025-05-10 19:13:38 +0300

    Update astro_calc.py

--- astro_calc.py
+++ astro_calc.py
@@ -13,9 +13,11 @@ POINTS = [const.ASC, const.MC]
 def calculate_natal(date_str, time_str, lat, lon):
     try:
         parsed = parser.parse(f"{date_str} {time_str}")
-        datetime = Datetime(parsed.strftime("%Y/%m/%d"), parsed.strftime("%H:%M"), '+00:00')
-        pos = GeoPos(lat, lon)
-        chart = Chart(datetime, pos, hsys='PL')  # <- Поменяли здесь P -> PL
+        date_fmt = parsed.strftime("%Y/%m/%d")
+        time_fmt = parsed.strftime("%H:%M")
+        datetime = Datetime(date_fmt, time_fmt, '+03:00')  # Москва
+        pos = GeoPos(str(lat), str(lon))
+        chart = Chart(datetime, pos, hsys=const.HOUSES_PLACIDUS)
 
         planets = []
         for obj in PLANETS + ['CHIRON', 'LILITH', 'SELENA', 'MEAN_NODE', 'PARTFORTUNE']:
