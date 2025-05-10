a290ef1e8675e5118cb9f325b7e15bae2832e0b8
NadezdaSev <nadezdavoice2025@gmail.com>
2025-05-10 18:53:08 +0300

    Add files via upload

--- astro_calc.py
+++ astro_calc.py
@@ -2,6 +2,8 @@ from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
 from flatlib import const
+from dateutil import parser
+
 PLANETS = [
     const.SUN, const.MOON, const.MERCURY, const.VENUS, const.MARS,
     const.JUPITER, const.SATURN, const.URANUS, const.NEPTUNE, const.PLUTO
@@ -9,27 +11,35 @@ PLANETS = [
 POINTS = [const.ASC, const.MC]
 
 def calculate_natal(date_str, time_str, lat, lon):
-    datetime = Datetime(date_str, time_str, '+00:00')
-    pos = GeoPos(lat, lon)
-    chart = Chart(datetime, pos, hsys='P')
+    try:
+        parsed = parser.parse(f"{date_str} {time_str}")
+        datetime = Datetime(parsed.strftime("%Y/%m/%d"), parsed.strftime("%H:%M"), '+00:00')
+        pos = GeoPos(lat, lon)
+        chart = Chart(datetime, pos, hsys='P')
 
-    planets = []
-    for obj in PLANETS + [POINTS.PartFortune, 'CHIRON', 'LILITH', 'SELENA', 'MEAN_NODE']:
-        try:
-            planet = chart.get(obj)
-            planets.append({
-                'name': planet.id,
-                'sign': planet.sign,
-                'degree': planet.lon,
-                'house': planet.house,
-            })
-        except Exception as e:
-            print(f"Ошибка при расчёте объекта {obj}: {e}")
+        planets = []
+        for obj in PLANETS + ['CHIRON', 'LILITH', 'SELENA', 'MEAN_NODE', 'PARTFORTUNE']:
+            try:
+                planet = chart.get(obj)
+                planets.append({
+                    'name': planet.id,
+                    'sign': planet.sign,
+                    'degree': planet.lon,
+                    'house': planet.house,
+                })
+            except Exception as e:
+                print(f"Ошибка при расчёте объекта {obj}: {e}")
 
-    return {
-        'mode': 'astro',
-        'planets': planets,
-        'ASC': chart.get('ASC').sign,
-        'MC': chart.get('MC').sign,
-        'message': '⚙️ Астрологический расчёт выполнен',
-    }
+        return {
+            'mode': 'astro',
+            'planets': planets,
+            'ASC': chart.get(const.ASC).sign,
+            'MC': chart.get(const.MC).sign,
+            'message': '⚙️ Астрологический расчёт выполнен',
+        }
+
+    except Exception as err:
+        return {
+            'error': f'Невозможно разобрать дату/время: {err}',
+            'mode': 'error'
+        }
